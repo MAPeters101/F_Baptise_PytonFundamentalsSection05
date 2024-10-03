1d6349816710ee2230f163ccb7366a5b882288aa
Mark Peters <mapeters101@earthlink.net>
2024-10-03 04:49:20 -0400

    Lesson046:  Work with lists.

---
+++ lists.py
@@ -0,0 +1,27 @@
+l = [10, 20, 30, 40, 50]
+print(l)
+print(type(l))
+print(l[0])
+print(l[1])
+print(l[4])
+print(len(l))
+#print(l[5])
+print(l[-1])
+print(l[-5])
+#print(l[-6])
+print(l[len(l)-1])
+l = []
+print(l)
+print(type[l])
+l = list()
+print(l)
+print(type[l])
+
+l = [10, 20, 30, 40, 50]
+print(l[2])
+l[2] = 3.14
+print(l)
+
+l[-1] = 'Hello'
+print(l)
+#l[5] = 'World'
