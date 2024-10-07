b012e9278be4dbe556700c02bdebb836b3a04db2
Mark Peters <mapeters101@earthlink.net>
2024-10-07 12:17:46 -0400

    Lesson058:  Work with unpacking sequences.

---
+++ UnpackingSequences.py
@@ -0,0 +1,34 @@
+rate = 5.0, 5.12
+print(rate[0], rate[1])
+apr, api = rate
+print(apr, api)
+a,b,c = 10,3.14,'abc'
+print(a,b,c)
+#a,b,c = 10,20
+#a,b,c = 10,20,30,40
+a = 10*2**3+5
+print(a)
+s = 'abcdef'
+a,b,c = (1+1, s[::-1], 3.14)
+print(s,a,b,c)
+
+a=100
+b=3.14
+print(f'a={a}  b={b}')
+
+tmp=a
+a=b
+b=tmp
+print(f'a={a}  b={b}')
+
+a=100
+b=3.14
+print(f'a={a}  b={b}')
+a,b = b,a
+print(f'a={a}  b={b}')
+
+
+
+
+
+
